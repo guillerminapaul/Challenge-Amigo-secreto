0b7dca5d5e0a051a64ba1f3d3fb9427778cfd908
Guillermina Paul <153566045+guillerminapaul@users.noreply.github.com>
2025-09-22 20:23:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-
 let amigos = [];
-let amigosRestantes = [];
 
-function agregarAmigo(){ 
+function agregarAmigo() {
     let input = document.getElementById("amigo");
     let nombre = input.value.trim();
-    
-    if (amigos.length >= 5){
-        alert("Solo puedes agregar 5 amigos.");
-        return;
-    }
-   
+
     if (nombre === "") {
-        alert ("Por favor, ingresa al menos dos nombres.");
+        alert("Por favor, inserte un nombre.");
         return;
     }
 
-    if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ'-]+$/.test(nombre)){
-        alert ("Por favor, ingresa solo letras sin espacios ni números.");
+    if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ'-]+$/.test(nombre)) {
+        alert("Por favor, ingrese solo letras sin números ni espacios.");
         return;
     }
 
@@ -25,17 +18,14 @@ function agregarAmigo(){
         alert("Este nombre ya fue agregado.");
         return;
     }
-        amigos.push(nombre);
-        amigosRestantes = [...amigos];
 
-        input.value = "";
-        input.focus();
-
-        actualizarListaAmigos();
-
+    amigos.push(nombre);
+    input.value = "";
+    input.focus();
+    actualizarListaAmigos();
 }
 
-function actualizarListaAmigos(){
+function actualizarListaAmigos() {
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
 
@@ -43,38 +33,7 @@ function actualizarListaAmigos(){
         let li = document.createElement("li");
         li.textContent = amigos[i];
         lista.appendChild(li);
-
     }
 }
 
-function sortearAmigo(){
-    
-
-    if (amigos.length <2){
-        alert("Ingresa al menos dos amigos, para sortear.");
-        return;
-    }
-
-    if (amigosRestantes.length === 0){
-        alert("Ya se han sorteado todos los amigos.");
-        document.getElementById("botonSortear").disabled = true;
-        document.getElementById("botonAñadir").disabled = true;
-        return;
-    }
-    
-
-
-    let indice = Math.floor(Math.random() * amigosRestantes.length);
-    let amigoSecreto = amigosRestantes[indice];
-
-    amigosRestantes.splice(indice, 1);
-
-    let resultado = document.getElementById("resultado");
-    resultado.innerHTML = `<li>Tu amigo secreto es: <strong>${amigoSecreto}</strong></li>`;
-    
-}
-
-
-
-
-
+function sortearAmigo()
